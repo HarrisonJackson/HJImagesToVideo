3f4978f6d930a4a0aa1089e3bd5d3760546f61bf
Harrison Jackson <harrison@doityourselfharry.com>
2013-08-04 20:47:16 -0500

    just files

---
+++ HJImagesToVideoDemo/HJImagesToVideoDemoTests/HJImagesToVideoDemoTests.h
@@ -0,0 +1,13 @@
+//
+//  HJImagesToVideoDemoTests.h
+//  HJImagesToVideoDemoTests
+//
+//  Created by Harrison Jackson on 8/4/13.
+//  Copyright (c) 2013 Harrison Jackson. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HJImagesToVideoDemoTests : SenTestCase
+
+@end
